7cde10ffa3e843ffe3ea63cdc3bd1462099ad5c3
DeltaLF <patricktsaikl@gmail.com>
2022-03-26 19:50:21 +0800

    template

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./header/Header";
+import About from "./about/About";
+import Experience from "./experience/Experience";
+import Project from "./project/Project";
+import Certificate from "./certificate/Certificate";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <About />
+        <Experience />
+        <Project />
+        <Certificate />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/certificate/Certificate.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Certificate extends React.Component {
+  render() {
+    return <h1>Certificate</h1>;
+  }
+}
+
+export default Certificate;
